02e4ef9632aaea349ad87cb05116d9339fb065a7
oa4ajp <mendiola_loyola@yahoo.com>
2018-07-23 20:10:44 -0500

    Interface Segregation Refactoring.

--- SOLID/04 Interface Segregation/IPrintTasks.cs
+++ SOLID/04 Interface Segregation/IPrintTasks.cs
@@ -4,23 +4,27 @@ namespace SOLID._04_Interface_Segregation
 {
     public interface IPrintTasks
     {
-        bool PrintContent(string content);
-        bool ScanContent(string content);
-        bool FaxContent(string content);
-        bool PhotoCopyContent(string content);
+        bool PrintContent(string content);        
         bool PrintDuplexContent(string content);
+    }
+
+    public interface IScanTasks
+    {
+        bool ScanContent(string content);
+    }
+
+    public interface IFaxTasks
+    {
+        bool FaxContent(string content);
+    }
+
+    public interface ICopyTasks
+    {
+        bool PhotoCopyContent(string content);
     }
 
     public class HPLaserJet : IPrintTasks
     {
-        public bool FaxContent(string content)
-        {
-            Console.WriteLine("Fax Done"); return true;
-        }
-        public bool PhotoCopyContent(string content)
-        {
-            Console.WriteLine("PhotoCopy Done"); return true;
-        }
         public bool PrintContent(string content)
         {
             Console.WriteLine("Print Done"); return true;
@@ -29,13 +33,9 @@ namespace SOLID._04_Interface_Segregation
         {
             Console.WriteLine("Print Duplex Done"); return true;
         }
-        public bool ScanContent(string content)
-        {
-            Console.WriteLine("Scan Done"); return true;
-        }
     }
 
-    public class CannonMG2470 : IPrintTasks
+    public class CannonMG2470 : IPrintTasks, IScanTasks, ICopyTasks
     {
         public bool PhotoCopyContent(string content)
         {
@@ -50,10 +50,6 @@ namespace SOLID._04_Interface_Segregation
             Console.WriteLine("Scan Done"); return true;
         }
         public bool PrintDuplexContent(string content)
-        {
-            return false;
-        }
-        public bool FaxContent(string content)
         {
             return false;
         }
